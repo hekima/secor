7d2476bdeea0bfa5ecfd7e73f334a50a5c9f31cc
Thiago Cardoso <thiagoncc@gmail.com>
2018-09-17 13:46:53 -0300

    Updating kafka version + minor adjustments

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,18 @@
 FROM openjdk:8
+
+RUN apt-get update \
+	&& apt-get install -y --no-install-recommends \
+		curl wget apt-transport-https libsnappy-dev libssl-dev libbz2-dev python-dev python-pip zip unzip python-setuptools\
+	&& rm -rf /var/lib/apt/lists/* \
+	&& apt-get clean
+RUN pip install --upgrade awscli
+
 # https://github.com/docker-library/openjdk/issues/145#issuecomment-334561903
 # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=894979
 RUN rm /etc/ssl/certs/java/cacerts ; update-ca-certificates -f
 RUN mkdir -p /opt/secor
 ADD target/secor-*-bin.tar.gz /opt/secor/
+RUN ln -s /opt/secor/secor-0.27-SNAPSHOT.jar /opt/secor/secor.jar
 
 COPY src/main/scripts/docker-entrypoint.sh /docker-entrypoint.sh
 RUN chmod +x /docker-entrypoint.sh
--- src/main/scripts/docker-entrypoint.sh
+++ src/main/scripts/docker-entrypoint.sh
@@ -1,135 +1,102 @@
 #!/bin/bash
+
+# based on https://github.com/ovotech/docker-secor
+
 set -e
 
-
-SECOR_CONFIG=''
-
-if [ -z "$ZOOKEEPER_QUORUM" ]; then
-    echo "ZOOKEEPER_QUORUM variable not set, launch with -e ZOOKEEPER_QUORUM=zookeeper:2181"
-    exit 1
-else
-    SECOR_CONFIG="$SECOR_CONFIG -Dzookeeper.quorum=$ZOOKEEPER_QUORUM"
-    echo "zookeeper.quorum=$ZOOKEEPER_QUORUM"
+if [[ ! -z "$DEBUG" ]]; then
+  set -x
 fi
 
-if [ -z "$ZOOKEEPER_PATH" ]; then
-    echo "ZOOKEEPER_PATH variable not set, launch with -e ZOOKEEPER_PATH=/"
-    exit 1
-else
-    SECOR_CONFIG="$SECOR_CONFIG -Dkafka.zookeeper.path=$ZOOKEEPER_PATH"
-    echo "kafka.zookeeper.path=$ZOOKEEPER_PATH"
+if [[ -z "$SECOR_GROUP" ]]; then
+  echo "You must specify the SECOR_GROUP variable."
+  echo "  e.g., launch with -e SECOR_GROUP=consumer-group-name"
+  exit 1
 fi
 
-if [[ ! -z "$KAFKA_SEED_BROKER_HOST" ]]; then
-    SECOR_CONFIG="$SECOR_CONFIG -Dkafka.seed.broker.host=$KAFKA_SEED_BROKER_HOST"
-    echo "kafka.seed.broker.host=$KAFKA_SEED_BROKER_HOST"
-fi
-if [[ ! -z "$KAFKA_SEED_BROKER_PORT" ]]; then
-    SECOR_CONFIG="$SECOR_CONFIG -Dkafka.seed.broker.port=$KAFKA_SEED_BROKER_PORT"
-    echo "kafka.seed.broker.port=$KAFKA_SEED_BROKER_PORT"
+# If BOTH are unset
+if [[ -z "$ZOOKEEPER_QUORUM" && -z "$KAFKA_SEED_BROKER_HOST" ]]; then
+  echo "You must set either ZOOKEEPER_QUORUM or KAFKA_SEED_BROKER_HOST."
+  echo "  e.g., launch with -e ZOOKEEPER_QUORUM=zookeeper:2181 or -e KAFKA_SEED_BROKER_HOST=my.kafka.host"
+  exit 1
 fi
 
-if [[ ! -z "$SECOR_GROUP" ]]; then
-    SECOR_CONFIG="$SECOR_CONFIG -Dsecor.kafka.group=$SECOR_GROUP"
-    echo "secor.kafka.group=$SECOR_GROUP"
+# If EITHER is unset
+if [[ -z "$SECOR_S3_BUCKET" || -z "$SECOR_S3_PATH" ]]; then
+  echo "You must set both SECOR_S3_BUCKET and SECOR_S3_PATH."
+  echo "  e.g., launch with -e SECOR_S3_BUCKET=my-bucket -e SECOR_S3_PATH=my-path"
+  exit 1
 fi
 
-
-if [[ ! -z "$AWS_REGION" ]]; then
-    SECOR_CONFIG="$SECOR_CONFIG -Daws.region=$AWS_REGION"
-    echo "aws.region=$AWS_REGION"
-fi
-if [[ ! -z "$AWS_ENDPOINT" ]]; then
-    SECOR_CONFIG="$SECOR_CONFIG -Daws.endpoint=$AWS_ENDPOINT"
-    echo "aws.endpoint=$AWS_ENDPOINT"
-fi
-if [[ ! -z "$AWS_PATH_STYLE_ACCESS" ]]; then
-    SECOR_CONFIG="$SECOR_CONFIG -Daws.client.pathstyleaccess=$AWS_PATH_STYLE_ACCESS"
-    echo "aws.client.pathstyleaccess=$AWS_PATH_STYLE_ACCESS"
-fi
-if [[ ! -z "$AWS_ACCESS_KEY" ]]; then
-    SECOR_CONFIG="$SECOR_CONFIG -Daws.access.key=$AWS_ACCESS_KEY"
-fi
-if [[ ! -z "$AWS_SECRET_KEY" ]]; then
-    SECOR_CONFIG="$SECOR_CONFIG -Daws.secret.key=$AWS_SECRET_KEY"
-fi
-if [[ ! -z "$AWS_SESSION_TOKEN" ]]; then
-    SECOR_CONFIG="$SECOR_CONFIG -Daws.session.token=$AWS_SESSION_TOKEN"
-fi
-if [[ ! -z "$SECOR_S3_BUCKET" ]]; then
-    SECOR_CONFIG="$SECOR_CONFIG -Dsecor.s3.bucket=$SECOR_S3_BUCKET"
-    echo "secor.s3.bucket=$SECOR_S3_BUCKET"
-fi
-if [[ ! -z "$S3_PATH" ]]; then
-    SECOR_CONFIG="$SECOR_CONFIG -Dsecor.s3.path=$S3_PATH"
-    echo "secor.s3.path=$S3_PATH"
-fi
-if [[ ! -z "$SECOR_SCHEMA_REGISTRY" ]]; then
-    SECOR_CONFIG="$SECOR_CONFIG -Dschema.registry.url=$SECOR_SCHEMA_REGISTRY"
-    echo "secor.s3.path=$S3_PATH"
+# If BOTH are set
+if [[ -n "$AWS_ACCESS_KEY" && -n "$AWS_SECRET_KEY" ]]; then
+  # if these vars don't get exported, AWS CLI will fall back to using its own credential lookup
+  export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY
+  export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY
 fi
 
-if [[ ! -z "$MESSAGE_TIMESTAMP_NAME" ]]; then
-    SECOR_CONFIG="$SECOR_CONFIG -Dmessage.timestamp.name=$MESSAGE_TIMESTAMP_NAME"
-    echo "message.timestamp.name=$MESSAGE_TIMESTAMP_NAME"
-fi
-if [[ ! -z "$MESSAGE_TIMESTAMP_NAME_SEPARATOR" ]]; then
-    SECOR_CONFIG="$SECOR_CONFIG -Dmessage.timestamp.name.separator=$MESSAGE_TIMESTAMP_NAME_SEPARATOR"
-    echo "message.timestamp.name.separator=$MESSAGE_TIMESTAMP_NAME_SEPARATOR"
-fi
-if [[ ! -z "$SECOR_PARSER_TIMEZONE" ]]; then
-    SECOR_CONFIG="$SECOR_CONFIG -Dsecor.parser.timezone=$SECOR_PARSER_TIMEZONE"
-    echo "secor.parser.timezone=$SECOR_PARSER_TIMEZONE"
-fi
+# Ensure we can access S3
+aws --region $AWS_REGION --endpoint-url $AWS_ENDPOINT s3 ls s3://$SECOR_S3_BUCKET > /dev/null
 
-if [[ ! -z "$CLOUD_SERVICE" ]]; then
-    SECOR_CONFIG="$SECOR_CONFIG -Dcloud.service=$CLOUD_SERVICE"
-    echo "cloud.service=$CLOUD_SERVICE"
-fi
-if [[ ! -z "$SECOR_UPLOAD_MANAGER_CLASS" ]]; then
-    SECOR_CONFIG="$SECOR_CONFIG -Dsecor.upload.manager.class=$SECOR_UPLOAD_MANAGER_CLASS"
-    echo "secor.upload.manager.class=$SECOR_UPLOAD_MANAGER_CLASS"
-fi
-if [[ ! -z "$SECOR_GS_BUCKET" ]]; then
-    SECOR_CONFIG="$SECOR_CONFIG -Dsecor.gs.bucket=$SECOR_GS_BUCKET"
-    echo "secor.gs.bucket=$SECOR_GS_BUCKET"
-fi
-if [[ ! -z "$SECOR_GS_PATH" ]]; then
-    SECOR_CONFIG="$SECOR_CONFIG -Dsecor.gs.path=$SECOR_GS_PATH"
-    echo "secor.gs.path=$SECOR_GS_PATH"
-fi
+cp /opt/secor/config/*.properties /opt/secor
 
-if [[ ! -z "$SECOR_MAX_FILE_BYTES" ]]; then
-    SECOR_CONFIG="$SECOR_CONFIG -Dsecor.max.file.size.bytes=$SECOR_MAX_FILE_BYTES"
-    echo "secor.max.file.size.bytes=$SECOR_MAX_FILE_BYTES"
-fi
-if [[ ! -z "$SECOR_MAX_FILE_SECONDS" ]]; then
-    SECOR_CONFIG="$SECOR_CONFIG -Dsecor.max.file.age.seconds=$SECOR_MAX_FILE_SECONDS"
-    echo "secor.max.file.age.seconds=$SECOR_MAX_FILE_SECONDS"
-fi
+SECOR_CONFIG_FILE=/opt/secor/secor.kubernetes-prod.properties
+LOG4J_CONFIG_FILE=/opt/secor/log4j.kubernetes-prod.properties
 
+# AWS Access Credentials
+if [[ -n "$AWS_ACCESS_KEY" ]]; then sed -i -e "s^aws.access.key=.*$^aws.access.key=${AWS_ACCESS_KEY}^" $SECOR_CONFIG_FILE ; fi
+if [[ -n "$AWS_SECRET_KEY" ]]; then sed -i -e "s^aws.secret.key=.*$^aws.secret.key=${AWS_SECRET_KEY}^" $SECOR_CONFIG_FILE ; fi
 
-if [[ ! -z "$SECOR_KAFKA_TOPIC_FILTER" ]]; then
-    SECOR_CONFIG="$SECOR_CONFIG -Dsecor.kafka.topic_filter=$SECOR_KAFKA_TOPIC_FILTER"
-    echo "secor.kafka.topic_filter=$SECOR_KAFKA_TOPIC_FILTER"
-fi
-if [[ ! -z "$SECOR_WRITER_FACTORY" ]]; then
-    SECOR_CONFIG="$SECOR_CONFIG -Dsecor.file.reader.writer.factory=$SECOR_WRITER_FACTORY"
-    echo "secor.file.reader.writer.factory=$SECOR_WRITER_FACTORY"
-fi
-if [[ ! -z "$SECOR_MESSAGE_PARSER" ]]; then
-    SECOR_CONFIG="$SECOR_CONFIG -Dsecor.message.parser.class=$SECOR_MESSAGE_PARSER"
-    echo "secor.message.parser.class=$SECOR_MESSAGE_PARSER"
-fi
-SECOR_CONFIG="$SECOR_CONFIG $SECOR_EXTRA_OPTS"
+# AWS S3 Endpoint Config
+if [[ -n "$AWS_REGION" ]]; then sed -i -e "s^aws.region=.*$^aws.region=${AWS_REGION}^" $SECOR_CONFIG_FILE ; fi
+if [[ -n "$AWS_ENDPOINT" ]]; then sed -i -e "s^aws.endpoint=.*$^aws.endpoint=${AWS_ENDPOINT}^" $SECOR_CONFIG_FILE ; fi
 
+# How to connect to Kafka/ZK
+if [[ -n "$KAFKA_SEED_BROKER_HOST" ]]; then sed -i -e "s/kafka.seed.broker.host=.*$/kafka.seed.broker.host=${KAFKA_SEED_BROKER_HOST}/" $SECOR_CONFIG_FILE ; fi
+if [[ -n "$KAFKA_SEED_BROKER_PORT" ]]; then sed -i -e "s/kafka.seed.broker.port=.*$/kafka.seed.broker.port=${KAFKA_SEED_BROKER_PORT}/" $SECOR_CONFIG_FILE ; fi
+if [[ -n "$ZOOKEEPER_QUORUM" ]]; then sed -i -e "s/zookeeper.quorum=.*$/zookeeper.quorum=${ZOOKEEPER_QUORUM}/" $SECOR_CONFIG_FILE ; fi
 
-cd /opt/secor
+# Where to store things in S3 and locally within the container
+sed -i -e "s/secor.s3.bucket=.*$/secor.s3.bucket=${SECOR_S3_BUCKET}/" $SECOR_CONFIG_FILE
+sed -i -e "s/secor.s3.path=.*$/secor.s3.path=${SECOR_S3_PATH}/" $SECOR_CONFIG_FILE
+sed -i -e "s/secor.local.path=.*$/secor.local.path=\/tmp\/${SECOR_GROUP}/" $SECOR_CONFIG_FILE
 
+# Which Kafka topics to listen to?
+if [[ -n "$SECOR_KAFKA_TOPIC_FILTER" ]]; then sed -i -e "s/secor.kafka.topic_filter=.*$/secor.kafka.topic_filter=${SECOR_KAFKA_TOPIC_FILTER}/" $SECOR_CONFIG_FILE ; fi
+if [[ -n "$SECOR_KAFKA_TOPIC_BLACKLIST" ]]; then sed -i -e "s/secor.kafka.topic_blacklist=.*$/secor.kafka.topic_blacklist=${SECOR_KAFKA_TOPIC_BLACKLIST}/" $SECOR_CONFIG_FILE ; fi
 
-DEFAULT_CLASSPATH="*:lib/*"
-CLASSPATH=${CLASSPATH:-$DEFAULT_CLASSPATH}
+# Max file size/ages
+if [[ -n "$SECOR_MAX_FILE_BYTES" ]]; then sed -i -e "s/secor.max.file.size.bytes=.*$/secor.max.file.size.bytes=${SECOR_MAX_FILE_BYTES}/" $SECOR_CONFIG_FILE ; fi
+if [[ -n "$SECOR_MAX_FILE_SECONDS" ]]; then sed -i -e "s/secor.max.file.age.seconds=.*$/secor.max.file.age.seconds=${SECOR_MAX_FILE_SECONDS}/" $SECOR_CONFIG_FILE ; fi
 
-java -Xmx${JVM_MEMORY:-512m} $JAVA_OPTS -ea -Dsecor_group=${SECOR_GROUP:-partition} -Dlog4j.configuration=file:${LOG4J_CONFIGURATION:-log4j.docker.properties} \
-        -Dconfig=${CONFIG_FILE:-secor.prod.partition.properties} $SECOR_CONFIG \
-        -cp $CLASSPATH ${SECOR_MAIN_CLASS:-com.pinterest.secor.main.ConsumerMain}
+# Output config
+if [[ -n "$SECOR_FILE_READER_WRITER_FACTORY" ]]; then sed -i -e "s/secor.file.reader.writer.factory=.*$/secor.file.reader.writer.factory=${SECOR_FILE_READER_WRITER_FACTORY}/" $SECOR_CONFIG_FILE ; fi
+if [[ -n "$SECOR_COMPRESSION_CODEC" ]]; then sed -i -e "s/secor.compression.codec=.*$/secor.compression.codec=${SECOR_COMPRESSION_CODEC}/" $SECOR_CONFIG_FILE ; fi
+if [[ -n "$SECOR_FILE_EXTENSION" ]]; then sed -i -e "s/secor.file.extension=.*$/secor.file.extension=${SECOR_FILE_EXTENSION}/" $SECOR_CONFIG_FILE ; fi
+
+# If using Timestamp parser
+if [[ -n "$SECOR_TIMESTAMP_NAME" ]]; then sed -i -e "s/message.timestamp.name=.*$/message.timestamp.name=${SECOR_TIMESTAMP_NAME}/" $SECOR_CONFIG_FILE ; fi
+if [[ -n "$SECOR_TIMESTAMP_PATTERN" ]]; then sed -i -e "s/message.timestamp.input.pattern=.*$/message.timestamp.input.pattern=${SECOR_TIMESTAMP_PATTERN}/" $SECOR_CONFIG_FILE ; fi
+
+# Partition files per hour/minute?
+if [[ -n "$PARTITIONER_GRANULARITY_HOUR" ]]; then sed -i -e "s/partitioner.granularity.hour=.*$/partitioner.granularity.hour=${PARTITIONER_GRANULARITY_HOUR}/" $SECOR_CONFIG_FILE ; fi
+if [[ -n "$PARTITIONER_GRANULARITY_MINUTE" ]]; then sed -i -e "s/partitioner.granularity.minute=.*$/partitioner.granularity.minute=${PARTITIONER_GRANULARITY_MINUTE}/" $SECOR_CONFIG_FILE ; fi
+
+# Consumer configuration
+if [[ -n "$SECOR_KAFKA_GROUP" ]]; then sed -i -e "s/secor.kafka.group=.*$/secor.kafka.group=${SECOR_KAFKA_GROUP}/" $SECOR_CONFIG_FILE ; fi
+if [[ -n "$SECOR_MESSAGE_PARSER_CLASS" ]]; then sed -i -e "s/secor.message.parser.class=.*$/secor.message.parser.class=${SECOR_MESSAGE_PARSER_CLASS}/" $SECOR_CONFIG_FILE ; fi
+if [[ -n "$SECOR_GENERATION" ]]; then sed -i -e "s/secor.generation=.*$/secor.generation=${SECOR_GENERATION}/" $SECOR_CONFIG_FILE ; fi
+if [[ -n "$SECOR_CONSUMER_THREADS" ]]; then sed -i -e "s/secor.consumer.threads=.*$/secor.consumer.threads=${SECOR_CONSUMER_THREADS}/" $SECOR_CONFIG_FILE ; fi
+if [[ -n "$SECOR_MESSAGES_PER_SECOND" ]]; then sed -i -e "s/secor.messages.per.second=.*$/secor.messages.per.second=${SECOR_MESSAGES_PER_SECOND}/" $SECOR_CONFIG_FILE ; fi
+if [[ -n "$SECOR_OFFSETS_PER_PARTITION" ]]; then sed -i -e "s/secor.offsets.per.partition=.*$/secor.offsets.per.partition=${SECOR_OFFSETS_PER_PARTITION}/" $SECOR_CONFIG_FILE ; fi
+if [[ -n "$KAFKA_OFFSETS_STORAGE" ]]; then sed -i -e "s/kafka.offsets.storage=.*$/kafka.offsets.storage=${KAFKA_OFFSETS_STORAGE}/" $SECOR_CONFIG_FILE ; fi
+if [[ -n "$KAFKA_DUAL_COMMIT_ENABLED" ]]; then sed -i -e "s/kafka.dual.commit.enabled=.*$/kafka.dual.commit.enabled=${KAFKA_DUAL_COMMIT_ENABLED}/" $SECOR_CONFIG_FILE ; fi
+
+if [[ -n "$SECOR_OSTRICH_PORT" ]]; then sed -i -e "s/ostrich.port=.*$/ostrich.port=${SECOR_OSTRICH_PORT}/" $SECOR_CONFIG_FILE ; fi
+
+java -Xmx${JVM_MEMORY:-512m} -ea \
+  -Dsecor_group=$SECOR_GROUP \
+  -Dlog4j.configuration=file:$LOG4J_CONFIG_FILE \
+  -Dconfig=$SECOR_CONFIG_FILE \
+  -cp /opt/secor/secor.jar:/opt/secor/lib/* \
+  com.pinterest.secor.main.ConsumerMain
